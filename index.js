a56fbbf2e1c7b5f3c0e647c30f7678d985a21a5e
vusalvabazade-lgtm <vusalvabazade@gmail.com>
2025-11-05 19:06:23 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once("ready", () => {
+  console.log(`Bot aktif: ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
