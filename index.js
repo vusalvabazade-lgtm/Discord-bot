426cc6f41866987c82f637d1bc9aa02166ce9158
vusalvabazade-lgtm <vusalvabazade@gmail.com>
2025-11-05 19:05:51 +0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.once('ready', () => {
-  console.log(`Bot aktif: ${client.user.tag}`);
-});
-
-client.login(process.env.TOKEN);
